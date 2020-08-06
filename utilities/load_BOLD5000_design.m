3becd0de0a29170303787ca0cc94e620908f2106
Jacob Prince <jacob.samuel.prince@gmail.com>
2020-08-06 13:41:25 -0400

    major design matrix bugfix

--- utilities/load_BOLD5000_design.m
+++ utilities/load_BOLD5000_design.m
@@ -1,28 +1,24 @@
-function [design, allses_design, labels] = load_BOLD5000_design(eventdir, sessionstorun)
-
-tic 
+function [design, allses_design, cond_labels] = load_BOLD5000_design(eventdir, sessionstorun)
 
 nses = 15;
-runimgs = 37;
+maxruns = 10;
 runtrs = 194;
 
-stimdur = 1;
 tr = 2;
 
-%% Accumulate event info for all sessions
+assert(isfolder(eventdir))
+
+%% step 1: get mapping from image name to condition number
 
 disp('accumulating event info...')
 
-allses_events = [];
+allses_events = cell(nses, maxruns);
+allses_design = cell(size(allses_events));
 
-ses_event_table = [];
-
-ses_nruns = [];
+stim_names = [];
 
 for ses = 1:nses
-    
-    absolute_run = 1;
-    
+        
     if ses < 10
         sesstr = ['0' num2str(ses)];
     else
@@ -36,129 +32,89 @@ for ses = 1:nses
     eventfiles = struct2table(dir(subeventdir));
     eventfiles = eventfiles(~eventfiles.isdir,:).name;
     eventfiles = eventfiles(contains(eventfiles,'events.tsv') & ~contains(eventfiles,'localizer'));
-    
-    ses_nruns(ses) = length(eventfiles);
-    
-    events = cell(1,length(eventfiles));
-    design = cell(1,length(eventfiles));
-    
-    nconditions = runimgs * length(design);
-    
-    for i = 1:length(eventfiles)
-        
-        % load event info, compute onset TRs
-        rundur = runtrs; %size(events{i},4);
-        events{i} = tdfread(fullfile(subeventdir,eventfiles{i}));
-        events{i}.ImgType = cell(size(events{i}.ImgType,1),1);
-        ses_event_table = [ses_event_table; struct2table(events{i})];
-        onsetTRs = round(events{i}.onset./tr)+1;
-        
-        % for now, treat each image as its own condition
-        conds = events{i}.Trial + (runimgs * (absolute_run-1));
-        
-        % sanity
-        assert(length(onsetTRs) == length(conds))
-        
-        % populate design matrix for that run
-        design{i} = sparse(rundur, nconditions);
-        
-        for j = 1:runimgs
-            design{i}(onsetTRs(j), conds(j)) = 1; % important, for single trial every entry gets its cond
-        end
-        
-        absolute_run = absolute_run + 1;
-        
-    end
-    
-    tmp = [];
-    for k = 1:length(events)
-        tmp = [tmp; events{k}.ImgName];
-    end
-    
-    allses_events{ses} = tmp;
-    
-end
-
-
-%% get indices of repeated images
-
-reps = zeros(size(ses_event_table,1),1);
-idx = 1;
-labels = zeros(size(ses_event_table,1),1);
-seen_imgs = {};
-
-for i = 1:size(ses_event_table,1)
-    
-    curr = ses_event_table(i,:).ImgName;
-    
-    if i > 1 && ismember(curr,seen_imgs)
-        repidx = find(ismember(seen_imgs,curr),1);
-        labels(i) = repidx;
-        reps(repidx) = reps(repidx) + 1;
-    else
-        labels(i) = idx;
-        reps(idx) = reps(idx) + 1;
-        idx = idx + 1;
-    end
-    
-    if i == 1
-        seen_imgs = {curr};
-    else
-        seen_imgs = [seen_imgs; curr];
+            
+    for run = 1:length(eventfiles)   
+       temp = tdfread(fullfile(subeventdir,eventfiles{run}));    
+       allses_events{ses,run} = temp;
+       stim_names = [stim_names; cellstr(temp.ImgName)];
     end
     
 end
 
-%% create design matrices where each image is its own condition (4916 total)
-% note: repeats do not get their own condition ID
+%%  
 
-allses_design = [];
+[unique_names,~,cond_labels] = unique(stim_names,'stable');
+mapping = [stim_names num2cell(cond_labels)];
+
+%%
 
 for ses = 1:nses
     
-    output_design = [];
-    
-    n = ses_nruns(ses);
-    
-    for i = 1:n
+    for run = 1:maxruns
         
-        runidx = ses_event_table.Sess == ses & ses_event_table.Run == i;
+        events0 = allses_events{ses,run};
         
-        conds = labels(runidx);
-        
-        % load event info, compute onset TRs
-        rundur = runtrs;
-        
-        onsetTRs = round(ses_event_table(runidx,:).onset./tr)+1;
-        
-        % sanity
-        assert(length(onsetTRs) == length(conds))
-        
-        % populate design matrix for that run
-        output_design{i} = sparse(rundur, length(unique(labels)));
-        
-        for j = 1:length(conds)
-            output_design{i}(onsetTRs(j), conds(j)) = 1; % important, for single trial every entry gets its cond
+        if ~isstruct(events0)
+            continue
         end
+        
+        design0 = sparse(runtrs, length(unique_names));
+
+        onsetTRs = round(events0.onset./tr)+1;
+        
+        % img names presented this run
+        runimgs = cellstr(allses_events{ses,run}.ImgName);
+        
+        % for each name 
+        for img = 1:length(runimgs)
+            
+            % use the mapping table to look up the img condition label
+            cond = cond_labels(strcmp(mapping(:,1),runimgs{img}));
+            
+            % assert that repeats (if exist) all have the same label
+            assert(all(cond == cond(1)))
+            
+            % insert label into design matrix at correct TR
+            design0(onsetTRs(img), cond(1)) = 1;
+            
+            %disp(['ses ' num2str(ses) ' run ' num2str(run) ': cond ' num2str(cond(1)) ' inserted at tr ' num2str(onsetTRs(img)) ': ' runimgs{img} '  (' unique_names{cond(1)} ')'])
+                
+        end
+        
+        allses_design{ses,run} = design0;
         
     end
     
-    allses_design{ses} = output_design;
-    
 end
+
+%%
+
+for ses = 1:nses
+    for run = 1:maxruns
+        if ~isstruct(allses_events{ses,run})
+            continue
+        end
+        % lookup img names using cond labels from design matrix 
+        [runconds, ~] = find(allses_design{ses,run}');
+        
+        namesA = unique_names(runconds);
+        namesB = cellstr(allses_events{ses,run}.ImgName);
+        
+        assert(all(strcmp(namesA,namesB)))
+   
+    end
+end
+
+%%
 
 design = [];
 
 for ses = sessionstorun
     
-    ses_design = allses_design{ses};
-    design = [design ses_design];
+    ses_design = allses_design(ses,:);
+    design = [design ses_design(~cellfun(@isempty,ses_design))];
 
 end
-
-disp('done')
-
-toc
 
 end
 
